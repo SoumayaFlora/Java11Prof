f008fab3b8c81a4f580a8cc10d0db68b12e01dcd
Simon Roberts <simon@dancingcloudservices.com>
2020-12-02 11:24:17 -0700

    Day One Break One

---
+++ src/main/java/usingvar/Ex1.java
@@ -0,0 +1,14 @@
+package usingvar;
+
+public class Ex1 {
+//  var count = 99; // NOT ALLOWED, local vars only
+  public static void x(String[] args) {
+//    var x;
+    var x = 99;
+    var y =
+        "hello";
+
+//    y = 99;
+    
+  }
+}
